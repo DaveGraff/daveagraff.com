a1c34f84a4d241a56cd76857d5f26f9770fd2673
unknown <daveagraff@gmail.com>
2020-05-21 15:13:25 -0400

    Created function to ignore old & boring repos

--- dist/loadrepos.js
+++ dist/loadrepos.js
@@ -8,25 +8,49 @@ jQuery.fn.loadRepositories = function(username) {
     var target = this;
 
     $.githubUser(username, function(data) {
-        var repos = data.data; // JSON Parsing
-        sortByName(repos);    
-        var list = $('<dl id="box-holder"/>');
+        var repos = sort_by_current(data.data); // JSON Parsing
+
+        //Remove uninteresting repos
+        repos = remove_boring(repos);
+
+        var list = $('<dl id="project-box"/>');
         target.empty().append(list);
         $(repos).each(function() {
-            langExtended = '<div class="languageNote color1text">' + this.language + '</div>'
+            langExtended = '<div class="languageNote">' + this.language + '</div>'
             description = this.description;
             if(description == null)
                 description = '';
             else
                 description = '<dd>' + description +'</dd>'
-            list.append('<div class="project-card color2background color5text"><h3><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a></h3>'+ (this.language ? langExtended:'') + description  + '</div>');
+            list.append('<div class="project-card"><h3><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a></h3>'+ (this.language ? langExtended:'') + description  + '</div>');
         });      
       });
       
-    function sortByName(repos) {
+    function sort_by_current(repos) {
         repos.sort(function(a,b) {
             //Sort by most recent
             return  Date.parse(b.pushed_at) - Date.parse(a.pushed_at);
        });
+
+        return repos;
+    }
+
+    function remove_boring(repos) {
+        boring = ['Algo-Practice', 'CSharp-Text-Game', 'MIPS-Projects', 'Small-Java-Projects']
+
+        remove = []
+        for(i = 0; i < repos.length; i++){
+            repo = repos[i].name;
+            console.log(repo)
+
+            if(boring.indexOf(repo) != -1){
+                remove.push(i);
+            }
+        }
+
+        for (index of remove.reverse())
+            repos.splice(index, 1);
+
+        return repos;
     }
 };
